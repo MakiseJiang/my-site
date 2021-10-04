1557fdbcc36e2f1ea3104bc1a6e6dade703cb086
MakiseJiang <MakiseJiang@outlook.com>
2021-10-04 21:35:00 +0800

    Add layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,18 @@
 // Step 1: Import React
+import { Link } from 'gatsby'
 import * as React from 'react'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
-  return (
-    <main>
-      <title>About Me</title>
-      <h1>About Me</h1>
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
-  )
+    return (
+    <Layout>
+        <title>About Me</title>
+        <h1>About Me</h1>
+        <Link to="/">Back to home</Link>
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+    )
 }
 
 // Step 3: Export your component
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return(
-    <main>
+    <Layout>
       <title>Home Page</title>
       <h1>This is my gatsby home Page!</h1>
+      <Link to="/about">About</Link>
       <p>I am making this by following the Gatsby tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
