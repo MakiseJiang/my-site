696f4d34540b9e555d91d55e67ffb0286dc196bb
MakiseJiang <MakiseJiang@outlook.com>
2021-10-04 22:32:21 +0800

    Add css modules and layouts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# 🚀This is a personal website developed based on Gatsby
+# Personal Website
+
+🚀 This is a personal website built to publish self introductions and blogs.
 
 It is still in progress... Hopefully I can finish it by this semester🤣!
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
-import { Link } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/layout'
 
 const IndexPage = () => {
   return(
-    <Layout>
-      <title>Home Page</title>
-      <h1>This is my gatsby home Page!</h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle='Home Page'>
       <p>I am making this by following the Gatsby tutorial.</p>
     </Layout>
   )
