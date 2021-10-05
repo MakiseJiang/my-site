5bda7a02660f2e0bf884270f829d3ea0e6019b8b
MakiseJiang <MakiseJiang@outlook.com>
2021-10-05 13:57:50 +0800

    Add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,33 @@
+import { graphql } from 'gatsby';
+import * as React from 'react';
+import Layout from '../components/layout'
+
+
+const BlogPage = ({ data }) => {
+    return(
+        <Layout pageTitle='Blog'>
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query{
+        allFile {
+            nodes {
+                name
+            }
+        }
+    }
+`
+
+
+export default BlogPage
