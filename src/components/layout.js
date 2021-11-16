9a171c28d7deff734d60ddf5ab18b4fe14c16ec8
MakiseJiang <MakiseJiang@outlook.com>
2021-11-16 15:40:20 +0800

    Add packages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,12 @@
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { 
     container,
     sidenav,
     profileAvatar,
+    textCenter,
+    centerAlignBox,
     background,
     heading,
     siteTitle
@@ -21,17 +24,22 @@ const Layout = ({pageTitle, children}) => {
     `)
     return(
         <div className={background}>
+        {/* SideBar */}
         <div className={sidenav}>
-        <div className={profileAvatar}></div>
-        <a><Link to='/'>Home</Link></a>
-        <a><Link to='/about'>About</Link></a>
-        <a><Link to='/blog'>Blog</Link></a>
+            <div className={profileAvatar}/>
+            <div className={textCenter}>Makise <strong>Jiang</strong></div>
+            <a href="#"><Link to='/'>Home</Link></a>
+            <a href="#"><Link to='/about'>About</Link></a>
+            <a href="#"><Link to='/blog'>Blog</Link></a>
+
         </div>
+        {/* MainPage */}
         <div className={container}>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
             <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <main>
                 <h1 className={heading}>{pageTitle}</h1>
+                <FontAwesomeIcon icon={['fab', 'apple']} color="black" />
                 {children}
             </main>
         </div>
